b5c70d86d714d5fd348a144281e4efd30ca006c9
Kurtis Moffett <moffkr91@gmail.com>
2018-04-09 15:02:36 -0700

    qs

--- Letter.js
+++ Letter.js
@@ -1,19 +1,28 @@
 
 
-let word_array = ['c', 'a', 't']
+let word_array = ['c']
 
 
 
 const return_char = (char) => {
-    
+let result = word_array.map((letter) => {
+            if (char != letter) {
+                char = '_'
+                return char
+            }
+            else if (char === letter) {
+                return char
+            }
+        })
+        return result
 }
 
 
 
 
 let Letter = function(char){
-    this.character = char.toString()
-    this.guess = false
+    this.character = char
+    this.guessed = false
     this.display_char = return_char(char)
     this.check_char = (param) => {
 
