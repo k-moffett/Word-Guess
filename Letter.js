b3eb7d7edd9dbb79e5e8def15b8a38e9ee0b68ae
Kurtis Moffett <moffkr91@gmail.com>
2018-04-10 08:36:39 -0700

    qs

--- Letter.js
+++ Letter.js
@@ -1,34 +1,31 @@
+class Letter {
+    constructor(letter, guessed) {
+        this.letter = letter
+        this.guessed = guessed
+    }
 
+    display_letter(letter) {
+        if (this.guessed === false) {
+            letter = '_'
+            return letter
+        }
+        else if (this.guessed === true) {
+            letter = this.letter
+            return letter
+        }
+    }
 
-let word_array = ['c', 'a', 't']
-
-
-
-const return_char = (char) => {
-let result = word_array.map((letter) => {
-            if (char != letter) {
-                char = '_'
-                return char
-            }
-            else if (char === letter) {
-                return char
-            }
-        })
-        return result
+    update_guessed(letter) {
+        if (this.letter === letter) {
+            this.guessed = true
+        }
+        else if (this.letter != letter) {
+            return this.guessed
+        }
+    
+    }
 }
 
-const update_guessed = () => {
-
-}
-
-let Letter = function(char){
-    this.character = char
-    this.guessed = false
-    this.display_char = return_char(char)
-    this.been_guessed = update_guessed(char)
-}
-
-let word = new Letter(word_array[0])
-console.log(word)
-
-
+let character = new Letter('s', true)
+console.log(character.display_letter())
+console.log(character.update_guessed())
